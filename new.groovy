36ede0dec1a1822965ae1383906a1b5e345597dc
GitKoti235 <31142253+GitKoti235@users.noreply.github.com>
2019-07-15 18:59:12 +0530

    Update new.groovy

--- new.groovy
+++ new.groovy
@@ -1,13 +1,8 @@
-
 def set_environment_variables(OS){
 //read from properties file
-def rootDir = pwd()
-def props = readProperties interpolate: true, file:"${rootDir}/variables.properties"
+//def rootDir = pwd()
+def props = readProperties interpolate: true, file:"variables.properties"
 
 env.clarity_version="${props["${OS}_CLARITY_VERSION"]}"
-
-}
-
-def OS='WINDOWS'
-set_environment_variables(OS)
 echo "${env.clarity_version}"
+}
